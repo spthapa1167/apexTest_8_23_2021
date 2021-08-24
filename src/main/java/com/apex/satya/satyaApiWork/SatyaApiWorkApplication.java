a17d236ae02b1c525ad34a953678bbc09b4c32ea
satya <satyapthapa@gmail.com>
2021-08-23 22:56:44 -0500

    initial load

---
+++ src/main/java/com/apex/satya/satyaApiWork/SatyaApiWorkApplication.java
@@ -0,0 +1,13 @@
+package com.apex.satya.satyaApiWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatyaApiWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatyaApiWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apex/satya/satyaApiWork/SatyaApiWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.satya.satyaApiWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatyaApiWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
